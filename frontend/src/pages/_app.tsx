725f1a4b1a8ed94faf1d53e82698c639fdefae76
cherrypopsicle <majd.khoury93@gmail.com>
2024-06-25 21:34:04 +0100

    metamask provider injected

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,14 +1,23 @@
-import { MetaMaskProvider } from "@/contexts/MetaMaskContext";
+import { MetaMaskProvider } from "@metamask/sdk-react";
 import "@/styles/globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <MetaMaskProvider>
-        <ChakraProvider>
-          <Component {...pageProps} />
-        </ChakraProvider>
+    <MetaMaskProvider
+      debug={false}
+      sdkOptions={{
+        dappMetadata: {
+          name: "Consensys PoC NFT Lending App",
+          url: "https://localhost:3000",
+        },
+        infuraAPIKey: "cewdkfj99ifaos",
+      }}
+    >
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
     </MetaMaskProvider>
   );
 }

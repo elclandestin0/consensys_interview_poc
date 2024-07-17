a0044fc19b3bd4288f2d70bb5e0bb21d893935a4
cherrypopsicle <majd.khoury93@gmail.com>
2024-07-17 12:04:01 +0100

    added stuff

--- frontend/src/components/TokenList.tsx
+++ frontend/src/components/TokenList.tsx
@@ -1,24 +1,23 @@
-import useCusdcContract from "@/hooks/contracts/useCusdcContract";
 import useERC721 from "@/hooks/contracts/useERC721";
-import { Box, Flex, Text, Stack, AccordionIcon } from "@chakra-ui/react";
+import { Box, Flex, Text, Stack } from "@chakra-ui/react";
 import { useSDK } from "@metamask/sdk-react";
 import { useEffect, useState } from "react";
 
 const TokenInfo = (token: any) => {
   const [balance, setBalance] = useState(null);
-  const { fetchBalance } = useERC721();
-  const { fetchTokenBalance } = useCusdcContract();
+  const { approvedTokens, fetchBalance } = useERC721();
   const { account } = useSDK();
 
   useEffect(() => {
-    const getBalance = async (symbol: any) => {
-      const balance = await fetchBalance(token.token.address, token.token.abi)
-      setBalance(balance);
+    const fetchTokenBalance = async (symbol: any) => {
+        // get the balance of the token here
+        const balance = await fetchBalance(token.token.address, token.token.abi);
+        setBalance(balance);
+      console.log(token);
     };
-    console.log(token);
 
-    getBalance(token.token.symbol);
-  }, [fetchBalance, account]);
+    fetchTokenBalance(token.token.symbol);
+  }, [approvedTokens, fetchBalance, account]);
 
   return (
     <Box p={4} borderWidth={1} borderRadius="lg" overflow="hidden" mb={4}>
